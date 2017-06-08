92c68b012029d45d457ad1a491e4834d1d10f9e8
Gordon <geniusgordon@gmail.com>
2017-06-08 17:59:06 +0800

    :construction: show qrocde bounds

---
+++ App/containers/QRCode/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import Camera from 'react-native-camera';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+  },
+  camera: {
+    flex: 1,
+  },
+  qrcode: {
+    position: 'absolute',
+    borderWidth: 3,
+    borderColor: 'green',
+  },
+});
+
+class QRCodeScanner extends Component {
+  static navigationOptions = {
+    header: null,
+  };
+  state = {
+    qrcodes: {},
+  };
+  handleBarCodeRead = e => {
+    if (this.state.qrcodes[e.data]) {
+      return;
+    }
+    this.setState({
+      qrcodes: {
+        ...this.state.qrcodes,
+        [e.data]: e,
+      },
+    });
+  };
+  renderQRCodeBounds = () => {
+    const { height, width } = Dimensions.get('window');
+    const qrcodes = Object.values(this.state.qrcodes);
+    return qrcodes.map(qrcode => {
+      const bounds = JSON.parse(qrcode.bounds);
+      const qrcodePositionStyle = {
+        top: height * bounds[1][1],
+        left: width * bounds[0][0],
+        height: height * (bounds[0][1] - bounds[1][1]),
+        width: width * (bounds[2][0] - bounds[0][0]),
+      };
+      return (
+        <View key={qrcode.data} style={[styles.qrcode, qrcodePositionStyle]} />
+      );
+    });
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          barCodeTypes={['qr']}
+          onBarCodeRead={this.handleBarCodeRead}
+          style={styles.camera}
+        />
+        {this.renderQRCodeBounds()}
+      </View>
+    );
+  }
+}
+
+export default QRCodeScanner;
--- App/index.js
+++ App/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import Camera from 'react-native-camera';
+import { StackNavigator } from 'react-navigation';
+import QRCode from './containers/QRCode';
 
-const App = () => (
-  <View
-    style={{
-      flex: 1,
-      backgroundColor: 'skyblue',
-    }}
-  >
-    <Camera
-      onBarCodeRead={e => {
-        console.log(e.data);
-        console.log(e.type);
-        console.log(e.bounds);
-      }}
-      style={{ flex: 1 }}
-    />
-  </View>
-);
+const App = StackNavigator({
+  QRCode: { screen: QRCode },
+});
 
 export default App;
