9fe193047861cdbb9b7c56f677020971385235f5
Gordon <geniusgordon@gmail.com>
2017-06-08 16:57:33 +0800

    :construction: add react-native-camera

--- App/index.js
+++ App/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Camera from 'react-native-camera';
 
 const App = () => (
-  <View>
-    <Text>QQ</Text>
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: 'skyblue',
+    }}
+  >
+    <Camera
+      onBarCodeRead={e => {
+        console.log(e.data);
+        console.log(e.type);
+        console.log(e.bounds);
+      }}
+      style={{ flex: 1 }}
+    />
   </View>
 );
 
